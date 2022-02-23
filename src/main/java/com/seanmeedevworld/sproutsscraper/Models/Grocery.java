e60ff94c3ed1a73c7f91ffa7ee4a30a787ea7217
SeanMee86 <seanmee8186@gmail.com>
2022-02-23 05:59:19 -0800

    remove application data

--- src/main/java/com/seanmeedevworld/sproutsscraper/Models/Grocery.java
+++ src/main/java/com/seanmeedevworld/sproutsscraper/Models/Grocery.java
@@ -3,10 +3,26 @@ package com.seanmeedevworld.sproutsscraper.Models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "sprouts")
 public class Grocery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "price")
     private double base_price;
+    @Column(name = "name")
     private String name;
-    private Category[] categories;
+    @Column(name = "size_string")
+    private String size_string;
+    @Column(name = "brand_name")
+    private String brand_name;
+//    @Column(name = "categories")
+//    private Category[] categories;
 }
